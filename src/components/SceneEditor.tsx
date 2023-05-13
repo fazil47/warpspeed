639e208c9bdcd07d5833cd472badaa3c8a1d34db
advaithca <advaith29042002@gmail.com>
2023-05-14 02:39:30 +0530

    Added html2canvas and converts to image

--- src/components/SceneEditor.tsx
+++ src/components/SceneEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   GizmoHelper,
   GizmoViewcube,
@@ -9,30 +9,34 @@ import {
   Sky,
 } from "@react-three/drei";
 import { useControls } from "leva";
-import { useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import html2canvas from "html2canvas";
+
+import { WebGLRenderer } from "three";
 
 import styles from "./SceneEditor.module.css";
 import SceneObjects from "@/components/SceneObjects";
 import FirstPersonControlsComponent from "@/components/FirstPersonControlsComponent";
 import { Vector3 } from "three";
+import { useRef } from "react";
+import html2canvas from "html2canvas";
 
 export default function SceneEditor() {
-  const canvas = useThree((state)=>state.gl.domElement)
+  const canvasRef = useRef(null);
 
+  // Function to convert the scene to an image
+  const convertToImage = () => {
+    const canvas = canvasRef.current;
+    const { width, height } = canvas.getBoundingClientRect();
 
-  useEffect(() => {
-    // Render the Three.js scene and convert it to an image
-    html2canvas(canvas).then((canvas2) => {
-      const imageData = canvas2.toDataURL();
-      const additionalData = {
-        numbers: [1, 2, 3],
-        strings: ['Hello', 'World'],
-      };
-      sendImageToServer(imageData, additionalData);
-    });
-  }, []);
+    html2canvas(canvas, { useCORS: true })
+      .then((canvas) => {
+        const dataUrl = canvas.toDataURL();
+        console.log(dataUrl); // You can now use the dataUrl or perform further processing
+      })
+      .catch((error) => {
+        console.error('Error capturing scene:', error);
+      });
+  };
+
 
   const ambientLightControls = useControls("Ambient Light", {
     color: "#ffffff",
@@ -40,36 +44,42 @@ export default function SceneEditor() {
   });
 
   return (
-    <Canvas className={styles.canvas}>
-      <FirstPersonControlsComponent />
-      <SceneObjects>
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="hotpink" />
-        </mesh>
-        <mesh position={[1, 0, 0]}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-        <pointLight position={[10, 10, 10]} />
-      </SceneObjects>
-      <Sky distance={450000} sunPosition={[0, 1, 0]} />
-      <ambientLight
-        color={ambientLightControls.color}
-        intensity={ambientLightControls.intensity}
-      />
-      <GizmoHelper
-        alignment="bottom-right" // widget alignment within scene
-        margin={[80, 80]} // widget margins (X, Y)
-        // onUpdate={() => {}}
-        // TODO: the selected object should be the target
-        onTarget={() => new Vector3(0, 0, 0)}
-        renderPriority={1}
-      >
-        <GizmoViewcube />
-      </GizmoHelper>
-      <Grid sectionColor="#000000" cellColor="#000000" />
-    </Canvas>
+    <>
+      <Canvas 
+        className={styles.canvas}
+        ref={canvasRef}
+        >
+        <FirstPersonControlsComponent />
+        <SceneObjects>
+          <mesh>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="hotpink" />
+          </mesh>
+          <mesh position={[1, 0, 0]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="orange" />
+          </mesh>
+          <pointLight position={[10, 10, 10]} />
+        </SceneObjects>
+        <Sky distance={450000} sunPosition={[0, 1, 0]} />
+        <ambientLight
+          color={ambientLightControls.color}
+          intensity={ambientLightControls.intensity}
+        />
+        <GizmoHelper
+          alignment="bottom-right" // widget alignment within scene
+          margin={[80, 80]} // widget margins (X, Y)
+          // onUpdate={() => {}}
+          // TODO: the selected object should be the target
+          onTarget={() => new Vector3(0, 0, 0)}
+          renderPriority={1}
+        >
+          <GizmoViewcube />
+        </GizmoHelper>
+        <Grid sectionColor="#000000" cellColor="#000000" />
+      </Canvas>
+      <button onClick={convertToImage}>Convert to Image</button>
+    </>
   );
 }
 function sendImageToServer(imageData: string, additionalData: { numbers: number[]; strings: string[]; }) {
