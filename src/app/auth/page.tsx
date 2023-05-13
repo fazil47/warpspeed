ddc28c7018fcd669a841b39da9b2b2f5f4f8416c
Fazil Babu <fazil47babu@gmail.com>
2023-05-13 20:06:00 +0530

    Added firebase dependencies

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { GoogleAuthProvider, EmailAuthProvider, Auth } from "firebase/auth";
+
 export default function Auth() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
