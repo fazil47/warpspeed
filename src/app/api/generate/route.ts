a1173339cfca6ea2cd45bd5af34a8e5e4b0fbb13
Fazil Babu <fazil47babu@gmail.com>
2023-05-13 19:07:18 +0530

    Basic image gen

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,3 +1,68 @@
-export async function POST(request: Request) {
-  return new Response("Hello, Next.js!");
+import fs from "node:fs";
+
+export async function POST(req: Request) {
+  const receivedformData = await req.formData();
+  console.log(receivedformData.get("image"));
+
+  const engineId = "stable-diffusion-v1-5";
+  const apiHost = receivedformData.get("apiHost") ?? "https://api.stability.ai";
+  const apiKey = process.env.STABILITY_API_KEY;
+
+  if (!apiKey) throw new Error("Missing Stability API key.");
+
+  const formData = new FormData();
+
+  // NOTE: This example is using a NodeJS FormData library.
+  // Browsers should use their native FormData class.
+  // React Native apps should also use their native FormData class.
+  formData.append("init_image", receivedformData.get("image") as Blob);
+  formData.append("init_image_mode", "IMAGE_STRENGTH");
+  formData.append("image_strength", receivedformData.get("strength") as string);
+  formData.append(
+    "text_prompts[0][text]",
+    receivedformData.get("prompt") as string
+  );
+  formData.append("cfg_scale", "7");
+  formData.append("clip_guidance_preset", "FAST_BLUE");
+  formData.append("samples", "1");
+  formData.append("steps", "30");
+
+  console.log(formData);
+
+  const response = await fetch(
+    `${apiHost}/v1/generation/${engineId}/image-to-image`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: formData,
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`Non-200 response: ${await response.text()}`);
+  }
+
+  interface GenerationResponse {
+    artifacts: Array<{
+      base64: string;
+      seed: number;
+      finishReason: string;
+    }>;
+  }
+
+  const responseJSON = (await response.json()) as GenerationResponse;
+
+  console.log(responseJSON);
+
+  responseJSON.artifacts.forEach((image, index) => {
+    fs.writeFileSync(
+      `v1_img2img_${index}.png`,
+      Buffer.from(image.base64, "base64")
+    );
+  });
+
+  return new Response("OK");
 }
--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,10 +1,71 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 import SceneEditor from "@/components/SceneEditor";
 
 export default function Editor() {
+  const [image, setImage] = useState<File>();
+  const [prompt, setPrompt] = useState<string>("");
+  const [strength, setStrength] = useState<number>(0.3);
+
+  async function generateImage(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!image) return;
+
+    const formData = new FormData();
+
+    formData.append("image", image);
+    formData.append("prompt", prompt);
+    formData.append("strength", strength.toString());
+    formData.append("apiHost", "https://api.stability.ai");
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      body: formData,
+    });
+
+    console.log(await response);
+  }
+
   return (
     <main className={styles.main}>
-      <SceneEditor />
+      {/* <SceneEditor /> */}
+      <form onSubmit={generateImage}>
+        <label>
+          Image Strength:
+          <input
+            type="number"
+            step={0.1}
+            value={strength}
+            onChange={(e) => {
+              setStrength(parseFloat(e.target.value));
+            }}
+          />
+        </label>
+        <label>
+          Text Prompt:
+          <input
+            type="text"
+            value={prompt}
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Image:
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              if (!e.target.files) return;
+              setImage(e.target.files[0]);
+            }}
+          />
+        </label>
+        <button type="submit">Generate</button>
+      </form>
     </main>
   );
 }
